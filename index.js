bdf1d07cd992c81746b053cad34f030a83d85c2a
Andres Vazquez <andres.vazquez.martin@gmail.com>
2024-02-05 05:55:38 +0100

    collision logic between projectiles and asteroids added

--- index.js
+++ index.js
@@ -158,7 +158,21 @@ window.setInterval(() => {
     })
   );
   console.log(asteroids);
-}, 3000);
+}, 1200);
+
+function circleCollision(circle1, circle2) {
+  const xDifference = circle2.position.x - circle1.position.x;
+  const yDifference = circle2.position.y - circle1.position.y;
+  const distance = Math.sqrt(
+    xDifference * xDifference + yDifference * yDifference
+  );
+
+  if (distance <= circle1.radius + circle2.radius) {
+    console.log('These have collided');
+    return true;
+  }
+  return false;
+}
 
 function animate() {
   window.requestAnimationFrame(animate);
@@ -194,6 +208,15 @@ function animate() {
       asteroid.position.y + asteroid.radius < 0
     ) {
       asteroids.splice(i, 1);
+    }
+
+    //projectiles collision
+    for (let j = projectiles.length - 1; j >= 0; j--) {
+      const projectile = projectiles[j];
+      if (circleCollision(asteroid, projectile)) {
+        asteroids.splice(i, 1);
+        projectiles.splice(j, 1);
+      }
     }
   }
 
